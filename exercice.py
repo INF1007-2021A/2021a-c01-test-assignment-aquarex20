eabf97595c55717664caabddfa53a901407e5efa
aquarex20 <78061100+aquarex20@users.noreply.github.com>
2021-09-02 14:31:42 -0400

    exercice infini

--- exercice.py
+++ exercice.py
@@ -4,3 +4,9 @@
 if __name__ == '__main__':
     #TODO: Modifier le code ici
     print("Le cours INF1007 est mon cours")
+    i=0
+    while i+1>i:
+        print("......................")
+        
+        
+       
